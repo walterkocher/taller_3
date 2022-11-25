6dae29f97388491f240a0fbd936cedf811e9a5ee
walter <w.kocher01@ufromail.cl>
2022-11-25 18:55:23 -0300

    final commit

--- src/main/java/Taller_3/Venta.java
+++ src/main/java/Taller_3/Venta.java
@@ -1,7 +1,14 @@
 package Taller_3;
 
 public class Venta {
-    public Venta( Cliente cliente, Inmueble inmueble, Vendedor vendedor){
+    public Cliente cliente;
+    public Inmueble inmueble;
+    public Vendedor vendedor;
 
+    public Venta( Cliente cliente, Inmueble inmueble, Vendedor vendedor){
+        this.cliente=cliente;
+        this.vendedor=vendedor;
+        this.inmueble=inmueble;
     }
+
 }
